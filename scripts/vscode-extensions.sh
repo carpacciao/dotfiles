5340db97a92235abf82bba307c21ceb552370c8d
carpacciao <43958708+carpacciao@users.noreply.github.com>
2024-04-15 16:55:50 +0200

    feat: changed light theme

--- scripts/vscode-extensions.sh
+++ scripts/vscode-extensions.sh
@@ -4,5 +4,6 @@ code --install-extension ms-vscode.atom-keybindings
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension enkia.tokyo-night
-code --install-extension mvllow.rose-pine
+code --install-extension GitHub.github-vscode-theme
 code --install-extension PKief.material-icon-theme
+code --install-extension tamasfe.even-better-toml
