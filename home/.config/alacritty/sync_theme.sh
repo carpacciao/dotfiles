419fcc72bf45c6c4623071b0918772b2ea113ca1
carpacciao <43958708+carpacciao@users.noreply.github.com>
2024-04-16 18:08:51 +0200

    refactor: alacritty sync theme

--- home/.config/alacritty/sync_theme.sh
+++ home/.config/alacritty/sync_theme.sh
@@ -3,15 +3,12 @@
 dark_theme="dark.toml"
 light_theme="light.toml"
 
-current_theme=""
-next_theme=""
+current_theme="$light_theme"
+next_theme="$dark_theme"
 
 if [ "$1" = "dark" ]; then
-  next_theme=$light_theme
-  current_theme=$dark_theme
-else  
-  next_theme=$dark_theme
-  current_theme=$light_theme
+  current_theme="$dark_theme"
+  next_theme="$light_theme"
 fi
 
 sed -i '' -e "s/$next_theme/$current_theme/g" ~/.config/alacritty/alacritty.toml
